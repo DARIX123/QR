7ded2c6fd4e10e8b7a98b7d2eb7033be0e234b57
medelsotomg-hub <medelsotomg@ag.cbtis225.edu.mx>
2025-10-23 14:46:16 -0600

    cambios de monica

--- mostrar_qr_visual.php
+++ mostrar_qr_visual.php
@@ -5,14 +5,30 @@ $archivo = $_GET['archivo'] ?? '';
 <html lang="es">
 <head>
   <meta charset="UTF-8">
+  <link rel="stylesheet" href="estiloqr.css">
   <title>QR Visual</title>
 </head>
 <body>
-  <h1>Escanea en tu celular para verificar tu cuenta</h1>
+  <div class="texto-arriba-container">
+    <div class="logo-arriba">
+      <img src="logo.png" alt="Logo">
+    </div>
+    <p class="texto-arriba">Cuatro personas, una comunidad, infinitas posibilidades</p>
+  </div>
+
+  <h2 class="titulo">TRAYECTORIA DE APRENDIZAJE</h2>
+
+  <p class="subtitulo">Acerque su dispositivo y desbloquee una animación exclusiva que transforma este momento en algo visualmente increíble.</p>
+
+  <h4 class="instruccion">Escanea el código QR</h4>
+
   <?php if($archivo): ?>
-      <img src="<?php echo htmlspecialchars($archivo); ?>" alt="Código QR">
+      <div class="qr-contenedor">
+        <img src="<?php echo htmlspecialchars($archivo); ?>" alt="Código QR" class="qr">
+      </div>
   <?php else: ?>
       <p>Error al cargar el QR</p>
   <?php endif; ?>
 </body>
 </html>
+
